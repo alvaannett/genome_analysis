f5109101b2c6bc68c8eda81c2ea25485b952ac7e
alvaannett <alva.annett@gmail.com>
2020-03-30 17:01:19 +0200

    final version

--- code/WGS_code/01_assembly.sh
+++ code/WGS_code/01_assembly.sh
@@ -1,30 +1,29 @@
-#!/bin/bash -l   
-#SBATCH -A g2020008 
-#SBATCH -p core 
-#SBATCH -n 2 
-#SBATCH -t 15:00:00 
-#SBATCH -J 01_assembly_L.ferr_WGS  
-#SBATCH --mail-type=ALL 
-#SBATCH --mail-user alva.anett@gmail.com 
+#!/bin/bash -l
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 15:00:00
+#SBATCH -J 01_assembly_L.ferr_WGS
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user Alva.Annett.4036@student.uu.se
 
-#Load modules 
-module load bioinfo-tools 
-module load canu/1.8 
+#Load modules
+module load bioinfo-tools
+module load canu/1.8
 
-#-------- RUN CANU --------------------------------- 
+#-------- RUN CANU ---------------------------------
 
-# -p prefix filenames 
+# -p prefix filenames
 # -d dir to run in
-# -pacbio-raw specifies the data type (we have raw PacBio reads...) 
-# genomeSize needs to be specified (taken from article) 
-# stopOnReadQuality as mentioned in the student manual 
+# -pacbio-raw specifies the data type (we have raw PacBio reads...)
+# genomeSize needs to be specified (taken from article)
+# stopOnReadQuality as mentioned in the student manual
 
 canu \
  -p WGS_assembly \
- -d WGS/01_assembly_out \
+ -d 01_assembly_out \
  genomeSize=2.6m \
  stopOnReadQuality=false \
- maxThreads=2 \  
- -pacbio-raw data/DNA_raw_data/ERR*.fastq.gz 
+ -pacbio-raw /domus/h1/alvaa/private/data/DNA_raw_data/ERR*.fastq.gz
 
 #--------------------------------------------------
