b3a323a98c93d15ca68a110d9deb888a0a4cdbd2
alvaannett <alva.annett@gmail.com>
2020-03-30 14:19:00 +0200

    final version

--- code/WGS_code/01_assembly.sh
+++ code/WGS_code/01_assembly.sh
@@ -2,28 +2,29 @@
 #SBATCH -A g2020008 
 #SBATCH -p core 
 #SBATCH -n 2 
-#SBATCH -t 00:00:00 
-#SBATCH -J 01_assembly  
+#SBATCH -t 15:00:00 
+#SBATCH -J 01_assembly_L.ferr_WGS  
 #SBATCH --mail-type=ALL 
 #SBATCH --mail-user alva.anett@gmail.com 
 
 #Load modules 
 module load bioinfo-tools 
-module load canu
+module load canu/1.8 
 
-#-------- run canu ---------------------------- 
+#-------- RUN CANU --------------------------------- 
 
 # -p prefix filenames 
 # -d dir to run in
-# -pacbio-raw specifies the data type 
+# -pacbio-raw specifies the data type (we have raw PacBio reads...) 
 # genomeSize needs to be specified (taken from article) 
-# stopOnReadQuality mentioned in the student manual
+# stopOnReadQuality as mentioned in the student manual 
 
 canu \
- -p WGSi_assembly \
- -d 01_assembly \
+ -p WGS_assembly \
+ -d WGS/01_assembly_out \
  genomeSize=2.6m \
  stopOnReadQuality=false \
- -pacbio-raw data/DNA_raw_data/ERR2028?.fastq.gz 
+ maxThreads=2 \  
+ -pacbio-raw data/DNA_raw_data/ERR*.fastq.gz 
 
-#----------------------------------------------
+#--------------------------------------------------
