3ae4b074faa2da012258645bd5e6dc0d681bd48e
alvaannett <alva.annett@gmail.com>
2020-03-31 15:36:21 +0200

    code for MUMmer plot

---
+++ code/WGS_code/04_quality_mummerplot.sh
@@ -0,0 +1,20 @@
+#!/bin/bash -l
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 00:10:00
+#SBATCH -J 04_mummmerplot
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user Alva.Annett.4036@student.uu.se
+
+#Load modules
+module load bioinfo-tools
+module load MUMmer/4.0.0beta2
+
+#------- RUN MUMMERPLOT -----------------------------
+
+mummerplot \
+ --prefix=04_quality_mummerplot_out \
+ /domus/h1/alvaa/private/WGS/03_numcmer/03_align_quality_nucmer_out.delta
+
+#----------------------------------------------------
