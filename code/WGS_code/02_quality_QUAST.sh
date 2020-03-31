afbdf68eb7d75d16528ad921bcb8c5fe9cfd9851
alvaannett <alva.annett@gmail.com>
2020-03-31 11:10:06 +0200

    bach file to run QUAST on UPPMAX, final version

---
+++ code/WGS_code/02_quality_QUAST.sh
@@ -0,0 +1,29 @@
+#!/bin/bash -l
+#SBATCH -A g2020008
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 00:20:00
+#SBATCH -J 02_quality_quast
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user Alva.Annett.4036@student.uu.se
+
+#Load modules
+module load bioinfo-tools
+module load quast/4.5.4
+
+#------- RUN QUAST --------------------------------
+# -o dir for output 
+# -R refrence genome (provided) 
+# -t number of threads 
+
+
+quast.py \
+ -o 02_quality_quast \
+ -R /domus/h1/alvaa/private/data/reference/OBMB01.fasta \
+ -t 2 \
+ /domus/h1/alvaa/private/WGS/01_assembly_out/WGS_assembly.contigs.fasta 
+
+
+#--------------------------------------------------
+
+
