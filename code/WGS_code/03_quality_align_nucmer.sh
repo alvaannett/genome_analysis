fa287ec00af37a44e8e3fa987c52d449713ccde6
alvaannett <alva.annett@gmail.com>
2020-04-01 17:44:59 +0200

    modified script to run shoow align and to produce a .fai file for plotting

--- code/WGS_code/03_quality_align_nucmer.sh
+++ code/WGS_code/03_quality_align_nucmer.sh
@@ -10,8 +10,10 @@
 #Load modules
 module load bioinfo-tools
 module load MUMmer/4.0.0beta2
+module load samtools/1.10
 
 #--------- RUN NUCMER -------------------------------------
+
 # -p prefic for output files 
 # --delta dir for output 
 # threads 
@@ -23,5 +25,16 @@ nucmer \
  --threads=2 \
  /domus/h1/alvaa/private/data/reference/OBMB01.fasta \
  /domus/h1/alvaa/private/WGS/01_assembly_out/WGS_assembly.contigs.fasta 
+
+#Takes output from nucmer and creates file with summary statistics etc...
+# -r sort output lines by reference
+# -c sequence length columns in the output
+# -l percent coverage columns in the output
+# -T tab delimited output 
+show-coords -r - c  -l -T \
+ 03_align_quality_nucmer_out.delta > 03_align_quality_nucmer_out.coords.txt 
+
+#create index fasta file from refrence genome for plotting 
+samtools faidx /domus/h1/alvaa/private/data/reference/OBMB01.fasta > OBMB01.fasta.fai
 
 #----------------------------------------------------------
